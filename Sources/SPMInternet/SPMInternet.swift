0cba77e49a97ebd0252512a97c069f66a90869c6
Ravisinh Mori <ravi@vebuin.com>
2023-03-23 15:37:32 +0530

    Initial Commit

--- Sources/SPMInternet/SPMInternet.swift
+++ Sources/SPMInternet/SPMInternet.swift
@@ -1,6 +1,15 @@
+
+import Alamofire
+
 public struct SPMInternet {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    public var reachability = NetworkReachabilityManager()
+    
+    public func checkInternet()->Bool {
+        return ((reachability?.isReachable) != nil)
     }
 }
